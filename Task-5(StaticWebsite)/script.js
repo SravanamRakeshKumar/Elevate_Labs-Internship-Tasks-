25ebf567902a08d4a04d51f45c2605b63fc3d010
Sravanam Rakesh Kumar <sravanamrakeshkumar3@gmail.com>
2025-08-11 20:42:12 +0530

    first commit

---
+++ Task-5(StaticWebsite)/script.js
@@ -0,0 +1,4 @@
+document.getElementById("magicBtn").addEventListener("click", function () {
+  const message = document.getElementById("message");
+  message.textContent = "🎉 You clicked the magic button!";
+});
